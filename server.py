eb93ce9b8887a7659ffff1923bc819f682fea726
Aryan Gaire <botx711@gmail.com>
2025-09-05 15:01:01 +0530

    fixed streaming results step wise

--- server.py
+++ server.py
@@ -32,35 +32,35 @@ async def ask_agent(request: QueryRequest):
             "sources_gathered": []
         }
 
-        result = graph.invoke(state, config=RunnableConfig()) #should instantiate with () otherwise we get error
+        #graph.invoke() will pause the entire execution until entire graph completes execution
+        #Intermediate Streaming results wont be possible because of it 
+        #result = graph.invoke(state, config=RunnableConfig()) #should instantiate with () otherwise we get error
         
-        #--This is the concept of SSE - Server Sent Events 
-        #Streaming Search queries 
-        for q in result.get("search_query", []):
-            payload = {"stage": "Generating Search Queries", "content": q}
-            yield f"event: step\ndata: {json.dumps(payload)}\n\n" #yeild used for sending partial responses 
-            await asyncio.sleep(0.3)
-        
-        #Streaming Web search results 
-        for r in result.get("web_research_result", []):
-            payload = {"stage": "Web Research", "content": r}
-            yield f"event: step\ndata: {json.dumps(payload)}\n\n"
-            await asyncio.sleep(0.3)
-        
-        #Stream reflection knowledge gap only if available
-        if "knowledge_gap" in result:
-            payload = {"stage": "Reflection", "content": result["knowledge_gap"]}
-            yield f"event: step\ndata: {json.dumps(payload)}\n\n"
-            await asyncio.sleep(0.3)
 
-        #Streaming sources(citations) 
-        for s in result.get("sources_gathered", []):
-            payload = {"stage": "Source", "content": s}
-            yield f"event: step\ndata: {json.dumps(payload)}\n\n"
-            await asyncio.sleep(0.3)
+        #--This is the concept of SSE - Server Sent Events 
+        async for event in graph.astream_events(state, config=RunnableConfig()):
+            if event["event"] == "on_chain_end" and "search_query" in event["data"]:
+                for q in event["data"]["search_query"]:
+                    payload = {"stage": "Generating Search Queries", "content": q}
+                    yield f"event: step\ndata: {json.dumps(payload)}\n\n"
+
+            elif event["event"] == "on_chain_end" and "web_research_result" in event["data"]:
+                for r in event["data"]["web_research_result"]:
+                    payload = {"stage": "Web Research", "content": r}
+                    yield f"event: step\ndata: {json.dumps(payload)}\n\n"
+
+            elif event["event"] == "on_chain_end" and "knowledge_gap" in event["data"]:
+                payload = {"stage": "Reflection", "content": event["data"]["knowledge_gap"]}
+                yield f"event: step\ndata: {json.dumps(payload)}\n\n"
+
+            elif event["event"] == "on_chain_end" and "sources_gathered" in event["data"]:
+                for s in event["data"]["sources_gathered"]:
+                    payload = {"stage": "Source", "content": s}
+                    yield f"event: step\ndata: {json.dumps(payload)}\n\n"
+
 
         #Streaming final answer 
-        final_answer = result["messages"][-1].content 
+        final_answer = event["data"]["messages"][-1].content 
         payload = {"stage": "Final Answer", "content": final_answer}
         yield f"event: final_answer\ndata: {json.dumps(payload)}\n\n"
         
